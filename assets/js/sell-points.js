b4def5f2ddc18dbab31d2ee6080dc92d3e20ae83
ellibeetherapy <55092488+h5avlov@users.noreply.github.com>
2020-07-16 19:50:46 +0300

    lightSlider applied to sell-points section

---
+++ assets/js/sell-points.js
@@ -0,0 +1,14 @@
+( function() {
+	// Initializes main slider
+	$( '.sell-points .entry-list' ).lightSlider( {
+		item: 1, 
+		pager: true 
+
+	} ); 
+
+	// Copies auto generated slider controls to text content wrapper in all slides
+	// const controls = $( '.sell-points .bx-controls-direction' ); 
+	// $( '.sell-points .text-content' )
+	// 	.append( controls ); 
+ 
+}() ); 
