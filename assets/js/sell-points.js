1e87b2e57f44a6fe0a63355df724a5fb899e5692
ellibeetherapy <55092488+h5avlov@users.noreply.github.com>
2020-07-19 00:46:06 +0300

    Slider settings updated

--- assets/js/sell-points.js
+++ assets/js/sell-points.js
@@ -2,6 +2,9 @@
 	// Initializes main slider
 	$( '.sell-points .entry-list' ).lightSlider( { 
 		auto: true, 
+		loop: true, 
+		pause: 7000, 
+		pauseOnHover: true, 
 		item: 1, 
 		pager: true, 
 		controls: false 
