3b5db791cbaf775779893d4ecf3508b69bac0f62
ellibeetherapy <55092488+h5avlov@users.noreply.github.com>
2020-08-08 17:53:12 +0300

    Switch language buttons hover effect added

--- assets/js/switch-lang.js
+++ assets/js/switch-lang.js
@@ -1,16 +1,25 @@
 ( function() { 
-	$( ".switch-lang-panel" ) 
+	const switchLangPanel = $( ".switch-lang-panel" ); 
+	let panelOpen = false; 
+
+	switchLangPanel 
 		.on( "click", function( e ) { 
-			const lang = $( e.target )
-				.attr( "data-lang" ); 
-
-			if ( lang === getPageLang() ) { 
-				return; 
-			}
-
-			Cookies.set( "lang", lang, { expires: 30 } ); 
-
-			redirectTo( lang );  
+			const target = $( e.target ); 
+			if ( target.is( ".open-switch-lang-panel-button" ) ) { 
+				if ( panelOpen === false ) { 
+					switchLangPanel.addClass( "panel-open" );
+				} else { 
+					switchLangPanel.removeClass( "panel-open" ); 
+				}
+				panelOpen = !panelOpen; 
+			} else if ( target.is( ".switch-lang-button" ) ) { 
+				const lang = target.attr( "data-lang" ); 
+				if ( lang === getPageLang() ) { 
+					return; 
+				}
+				Cookies.set( "lang", lang, { expires: 30 } ); 
+				redirectTo( lang );  
+			} 
 
 		} ); 
 }() );
