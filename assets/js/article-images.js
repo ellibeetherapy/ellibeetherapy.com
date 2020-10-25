d2b8e0b3c42f78a84e17690b47d074bf03d33112
h5avlov <h5avlov@abv.bg>
2020-10-25 16:53:56 +0200

    MagnificPopup gallery plugin installed, loaded and set

--- assets/js/article-images.js
+++ assets/js/article-images.js
@@ -5,5 +5,19 @@
 	// 	pager: false, 
 	// 	controls: true 
 	// } ); 
+
+	// $( '.image-entry-list.multiple-images' ).bxSlider( { 
+	// 	minSlides: 2, 
+	// 	maxSlides: 2, 
+	// 	// shrinkItems: true, 
+	// 	pager: false, 
+	// 	controls: true 
+	// } ); 
+
+	$( '.image-entry-list.multiple-images' ).magnificPopup( { 
+		delegate: 'a', 
+		type: 'image' 
+
+	} ); 
  
 }() ); 
