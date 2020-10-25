6bdd6595dfa2e9998e5bd671fdbb536784ee66d3
h5avlov <h5avlov@abv.bg>
2020-10-25 14:28:57 +0200

    flexslider and bxSlider added, bxSlider loaded and set

--- assets/js/article-images.js
+++ assets/js/article-images.js
@@ -1,9 +1,9 @@
 ( function() {
 	// Initializes article images slider
-	$( '.image-entry-list.multiple-images' ).lightSlider( { 
-		item: 2, 
-		pager: false, 
-		controls: true 
-	} ); 
+	// $( '.image-entry-list.multiple-images' ).lightSlider( { 
+	// 	item: 2, 
+	// 	pager: false, 
+	// 	controls: true 
+	// } ); 
  
 }() ); 
--- assets/js/sell-points.js
+++ assets/js/sell-points.js
@@ -1,18 +1,37 @@
 ( function() {
 	// Initializes main slider
-	$( '.sell-points .entry-list' ).lightSlider( {  
-		mode: 'slide', 
-		vertical: true, 
-		adaptiveHeight: true,
-		pager: true, 
+	// $( '.sell-points .entry-list' ).lightSlider( {  
+	// 	mode: 'slide', 
+	// 	vertical: true, 
+	// 	adaptiveHeight: true,
+	// 	pager: true, 
+	// 	controls: false, 
+	// 	pause: 8000,
+	// 	auto: true, 
+	// 	// loop: true, 
+	// 	item: 1, 
+	// 	slideEndAnimation: false,  
+	// 	// pauseOnHover: true,   
+	// 	keyPress: true 
+	// } ); 
+
+	// $('.sell-points').flexslider( { 
+	// 	animation: 'slide', 
+	// 	direction: 'vertical' 
+
+	// } ); 
+
+	$('.sell-points .entry-list').bxSlider( { 
+		mode: 'vertical', 
+		adaptiveHeight: true, 
 		controls: false, 
-		pause: 8000,
+		preloadImages: 'all', 
 		auto: true, 
-		// loop: true, 
-		item: 1, 
-		slideEndAnimation: false,  
-		// pauseOnHover: true,   
-		keyPress: true 
+		autoStart: true, 	
+		autoHover: true, 
+		infiniteLoop: false, 
+		pause: 7000, 
+		touchEnabled: false 
 	} ); 
  
 }() ); 
